45e5f14dabc591ecea4585d7227744ec4e37a1d3
study.todair@gmail.com <->
2026-07-28 16:43:01 +0900

    python - if, for, while

--- workspace_python/_08_if.py
+++ workspace_python/_08_if.py
@@ -12,3 +12,64 @@ if True :
 
 if True :
     pass
+else :
+    pass
+
+if 1 :
+    print('참')
+
+'''
+파이썬에서 False란?
+False, 
+None, 
+0, 0.0, 
+빈 컨테이너(비어있는 문자열, 리스트, 튜플, 딕셔너리)
+'''
+
+a = []
+if a :
+    print('참')
+else :
+    print('거짓')
+
+# 174p. 문제 14.7
+score = input('점수 4개 입력, 띄어쓰기로 구분 : ')
+print(score, score.split(' '))
+scores = score.split(' ')
+sum = int(scores[0]) + int(scores[1]) + int(scores[2]) + int(scores[3])
+avg = sum / len(scores)
+
+if (0 <= int(scores[0]) <= 100) \
+    and (0 <= int(scores[1]) <= 100) \
+    and (0 <= int(scores[2]) <= 100) \
+    and (0 <= int(scores[3]) <= 100) :
+
+    if avg >= 80 :
+        print('합격')
+    else :
+        print('불합격')
+else :
+    print('잘못된 입력')
+
+# 178p. 1-콜라 2-사이다 3-환타 그 외-메뉴 없음
+button = int(input('메뉴를 고르시오'))
+if button == 1 :
+    print('콜라')
+elif button == 2 :
+    print('사이다')
+elif button == 3 :
+    print('환타')
+else :
+    print('다시 고르시오')
+
+# break 필요 없음
+# 또는은 | (파이프)
+a = 7
+match a :
+    case 6 | 7 | 8 :
+        print('여름')
+    case '여름2' :
+        print('여름2')
+    case _ :
+        print('그 외')
+
---
+++ workspace_python/_09_for.py
@@ -0,0 +1,76 @@
+for i in range(5) :
+    print(i, end=' ')
+print()
+for i in reversed(range(5)) :
+    print(i, end=' ')
+
+
+print('-'*30)
+# 구구단
+# j = 2
+# for i in range(1, 9+1) :
+#     print(f'2x{i}={2*i}')
+# j = 3
+# for i in range(1, 9+1) :
+#     print(f'{j}x{i}={j*i}')
+
+for j in range(2, 9+1) :
+    for i in range(1, 9+1) :
+        print(f'{j}x{i}={j*i}')
+
+
+print('-'*30)
+# 구구단인데 3단씩 옆으로
+'''
+2x1=2  3x1=3  4x1=4
+...
+
+5x1=5  6x1=6  7x1=7
+...
+
+8x1=8  9x1=9
+'''
+
+# i = 2, 5, 8
+i = 2
+j = range(1, 10)
+# for j in range(1, 10) :
+#     print(f'{i}x{j}={i*j}  {i+1}x{j}={(i+1)*j}  {i+2}x{j}={(i+j)*1}')
+
+# for i in range(2, 10, 3) :
+#     for j in range(1, 10) :
+#         if i+2 < 10 :
+#             print(f'{i}x{j}={i*j}  {i+1}x{j}={(i+1)*j}  {i+2}x{j}={(i+j)*1}')
+#         else :
+#             print(f'{i}x{j}={i*j}  {i+1}x{j}={(i+1)*j}')
+
+k = 14
+for i in range(2, k+1, 3) :
+    for j in range(1, 9+1) :
+        # print(f'{i}x{j}={i*j}', end='  ') 
+        # if i+1 <= k :
+        #     print(f'{i+1}x{j}={(i+1)*j}', end='  ')
+        # if i+2 <= k :
+        #     print(f'{i+2}x{j}={(i+2)*j}', end='  ')
+        # print()
+        for m in range(3) :
+            if i+m <= k :
+                print(f'{i+m}x{j}={(i+m)*j}', end='\t') 
+        print()
+    print()
+
+
+import random
+print( random.random() )
+print( random.randint(1, 6) )
+
+print('-'*12)
+# 주사위 3이 몇번만에 나오는지 출력
+dice = -1
+count = 0
+while dice != 3 :
+    dice = random.randint(1, 6)
+    count += 1
+    if dice == 3 :
+        print(count)
+
