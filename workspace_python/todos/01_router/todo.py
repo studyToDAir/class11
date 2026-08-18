da3f796cea90a0ef82fe6cc27b274aedc64c284a
study.todair@gmail.com <->
2026-08-18 12:32:55 +0900

    python - pydantic

--- workspace_python/todos/01_router/todo.py
+++ workspace_python/todos/01_router/todo.py
@@ -4,6 +4,9 @@ from model import Todo
 
 from fastapi import APIRouter
 todo_router = APIRouter()
+
+todo_list = []
+
 @todo_router.post('/todo')
 async def add_todo(todo:dict) -> dict :
     print('todo:', todo)
