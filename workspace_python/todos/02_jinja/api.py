aad955b8da860efb1bc2b21a92fbd3c1c3ad090a
study.todair@gmail.com <->
2026-08-20 17:16:42 +0900

    python - jinja

--- workspace_python/todos/02_jinja/api.py
+++ workspace_python/todos/02_jinja/api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory='templates/')
 
 @app.get('/hello')
@@ -11,6 +12,36 @@ def hello(request:Request) :
         'ip': request.client.host,
         'msg': '안뇽?'
     })
+
+@app.get('/youtube')
+def youtube(request:Request) :
+    print('/youtube 실행')
+
+    return templates.TemplateResponse(request, 'youtube.html', {
+        'like': 3,
+        'star': 1.5,
+        'bookmark': ['동영상1', '동영상2', '동영상3', '동영상4', '동영상5' ]
+    })
+
+def price(value) :
+    return f'{ int(value) :,}'
+# 사용자 필터 만들고 적용하기
+# ['price'] : Jinja에서 사용할 필터 이름
+templates.env.filters['price'] = price
+
+# 날짜 포멧
+from datetime import datetime
+def format_data(value, format="%Y-%m-%d %H:%M:%S"):
+    v = datetime.fromisoformat(value)
+    return v.strftime(format)
+templates.env.filters['format_data'] = format_data
+
+# textarea의 엔터 문자인 \n을 <br>로 바꾸고 HTML로 인식 시키는 필터
+
+def n2br(value):
+    from markupsafe import Markup # innerHTML로 만들어 주는 모듈
+    return Markup(value.replace('\n', '<br>'))
+templates.env.filters['n2br'] = n2br
 
 if __name__ == "__main__":
     import uvicorn
---
+++ workspace_python/todos/03_response/api.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, Request
+app = FastAPI()
+
+
+@app.get('/step1')
+def step1(request:Request) :
+    print('/step1 실행')
+    data = request.query_params
+    item = data.get('item')
+    print(f'item: {item}')
+
+    print('너무 복잡하고 정교해서 복붙하긴 좀 그런 곳')
+
+@app.get('/step2')
+def step2(request:Request) :
+    print('/step2 실행')
+    data = request.query_params
+    item = data.get('item')
+    print(f'item:{item} 관련 일 처리는 끝났고')
+
+    print('step1으로 이동')
+    # 1. forward 방식
+    step1(request)
+
+from fastapi.responses import RedirectResponse
+@app.post('/step3')
+def step3(request:Request) :
+    print('/step3 실행')
+    data = request.query_params
+    item = data.get('item')
+    print(f'item:{item} 관련 일 처리는 끝났고')
+
+    print('step1으로 이동')
+    # 2. redirect 방식
+    return RedirectResponse(
+        url='/step1',
+        status_code=303 # 기본값: 307
+    )
+    # 303 : 다시 올 때 Get으로 접속
+    # 307 : 다시 올 때 원래 방식 유지
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("api:app", port=8000, reload=True, host="0.0.0.0")
